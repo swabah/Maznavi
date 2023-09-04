c2baa12250e6508320b68b0be09bd63a9273a008
Sw_abah <ahmedswabah922@gmail.com>
2023-09-04 12:38:15 -0400

    removed the manifest

--- src/components/posts/SingleArticle.jsx
+++ src/components/posts/SingleArticle.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {useAuth} from "../../hooks/auths";
-import { useArticleToggleLike} from "../../hooks/posts";
 import { PiArrowUpRightBold,PiArrowLineUpRightThin } from "react-icons/pi";
 import PostsDemo from "../Demo/PostsDemo";
 import { ReadingTime } from "../../assets/ReadingTime";
 import { AiOutlineRead } from "react-icons/ai";
-import { RxReader } from "react-icons/rx";
 
 export const SingleArticle = ({Article,Loading,key}) => {
-  const {user, isLoading: authLoading} = useAuth();
-  const { id, likes , uid } = Article;
-  const isLiked = likes.includes(user?.id);
-  const {toggleArticleLike, isLoading} = useArticleToggleLike({
-    id,
-    isLiked,
-    uid: user?.id,
-  });
 
 
   const imageUrl = Article.imageUrl;
@@ -28,35 +17,35 @@ export const SingleArticle = ({Article,Loading,key}) => {
     {Loading ? (
       <PostsDemo h="h-96" count={20} />
     ) : (
-    <div key={key} className="w-full h-full gap-2 md:gap-4 flex flex-col">
+    <div key={key} className="flex flex-col w-full h-full gap-2 md:gap-4">
         <div className='w-full h-full'>
           <Link
            to={`/Articles/id/${Article?.id}`}
-           className='relative h-full w-full '
+           className='relative w-full h-full '
            onClick={()=>window.scrollTo({top: 0, left: 0, behavior: 'smooth'})}
           >
-           <img src={imageUrl} alt="" className="w-full h-52 hover:scale-110 duration-300 rounded-t-lg transition-all object-cover" />
+           <img src={imageUrl} alt="" className="object-cover w-full transition-all duration-300 rounded-t-lg h-52 hover:scale-110" />
            <div className='absolute inset-0 w-full h-full bg-[#0000003b] rounded-t-lg flex items-center opacity-0 hover:opacity-100 transition-all justify-center text-white text-5xl'><PiArrowLineUpRightThin/></div>
            <div className='absolute backdrop-blur text-sm md:text-base bg-opacity-40 top-2 left-2 z-20 w-auto h-auto text-white bg-[#000] rounded-md flex items-center gap-2 p-1 px-2 '>
             <AiOutlineRead/>
-            <h2 className='text-xs font-sans font-extralight'>{ReadingTime(Article?.content)} minutes</h2>
+            <h2 className='font-sans text-xs font-extralight'>{ReadingTime(Article?.content)} minutes</h2>
            </div>
           </Link>
         </div>
         <div className=" text-[#3f2d23] h-full w-full gap-1 lg:gap-1.5 flex flex-col justify-start items-start ">
-                <div className='opacity-90 w-full flex gap-1 flex-row justify-start items-center '>
+                <div className='flex flex-row items-center justify-start w-full gap-1 opacity-90 '>
                   <Link onClick={()=>window.scrollTo({top: 0, left: 0, behavior: 'smooth'})} to={`/Articles/${Article?.topic}`}>
-                     <h2 className=" uppercase font-semibold">{Article.topic}</h2>
+                     <h2 className="font-semibold uppercase ">{Article.topic}</h2>
                   </Link>
                   |
                   <p className="text-xs md:text-base text-[#3f2d23] "> {Article.created.date}</p>
                 </div>
                <Link
                   to={`/Articles/id/${Article?.id}`}
-                  className='flex items-center hover:underline justify-between w-full'
+                  className='flex items-center justify-between w-full hover:underline'
                   onClick={()=>window.scrollTo({top: 0, left: 0, behavior: 'smooth'})}
                 >
-                    <h2 className='text-lg md:text-xl w-11/12 truncate'>{Article.title}</h2>
+                    <h2 className='w-11/12 text-lg truncate md:text-xl'>{Article.title}</h2>
                     <PiArrowUpRightBold className="md:text-lg"/>
                 </Link>
         </div>
