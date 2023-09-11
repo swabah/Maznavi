1114cf8d22ce8850949360e8d6c0c5013e51106e
swabah <ahmedswabah922@gmail.com>
2023-09-11 15:51:35 +0530

    Unexpected token, expected

--- src/components/Pages/CurrentPage/CurrentProfile.jsx
+++ src/components/Pages/CurrentPage/CurrentProfile.jsx
@@ -92,12 +92,12 @@ function CurrentProfile() {
                 <div  onClick={() => onViewOpen()} className='col-span-1 p-1 cursor-pointer px-5 rounded-md bg-gray-100 hover:bg-gray-200'>
                 View Profile
                 </div>
-                // <div
-                // onClick={() => onShareOpen()}
-                // className='col-span-1 p-1 cursor-pointer px-5 rounded-md bg-gray-100 hover:bg-gray-200'
-                // >
-                // Share Profile
-                // </div>
+                <div
+                onClick=''
+                className='col-span-1 p-1 cursor-pointer px-5 rounded-md bg-gray-100 hover:bg-gray-200'
+                >
+                Share Profile
+                </div>
                 {CurrentUser?.username === user?.username && (
                   <div
                     onClick={() => onUpdateOpen()}
